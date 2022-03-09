90ae2913b212c2421fbeb6615ea821bcfb35c684
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-03-08 19:41:13 -0500

    created Kourse model

---
+++ RestaurantRater/Models/Kourse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Kourse
+    {
+        public int KourseId { get; set; }
+        public string Name { get; set; }
+        public string Cup { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class KourseDbContext : DbContext
+    {
+        public DbSet<Kourse> Koursees { get; set; }
+    }
+}
