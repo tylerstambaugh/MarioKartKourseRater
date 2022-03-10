d9d39b763e9f2bb3fa5a0feb20f6b48fb8d216e2
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-03-10 17:02:19 -0500

    finished crud operaions throughout the stack

--- RestaurantRater/Controllers/KourseController.cs
+++ RestaurantRater/Controllers/KourseController.cs
@@ -1,7 +1,9 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -41,6 +43,7 @@ namespace RestaurantRater.Controllers
                 {
                     //Should show a successs page
                     return Redirect("/Kourse");
+                    //return RedirectToAction("Index");
                 }
                 ViewData["Error"] = "Couldn't create that course.";
                 return View(model);
@@ -52,7 +55,8 @@ namespace RestaurantRater.Controllers
             }
         }
 
-        public ActionResult Details(int id)
+        //GET: /kourse/Details/[id]
+        public ActionResult Details(int? id)
         {
             var context = new KourseDbContext();
             var entity = context.Kourses.Find(id);
@@ -64,38 +68,96 @@ namespace RestaurantRater.Controllers
             return Redirect("/kourse");
         }
 
+        //GET: /Kourse/Edit/{id}
         //for the base view, prior to posting the update to the controller
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public ActionResult Edit(int? id)
         {
-            var context = new KourseDbContext();
-            var entity = context.Kourses.Find(id);
-            if (entity != null)
+            if (id == null)
             {
-                return View(entity);
+                //return 400
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //return 404 page
-            return Redirect("/kourse");
+            var context = new KourseDbContext();
+            var kourse = context.Kourses.Find(id);
+            if (kourse != null)
+            {
+                return View(kourse);
+            }
+            //return 404 
+            return HttpNotFound();
         }
 
+        //POST: /kourse/edit{id}
         //the actual edit 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, Kourse model)
         {
+            var context = new KourseDbContext();
 
-            ////if(model.KourseId != id)
+            if(model.KourseId != id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if(!ModelState.IsValid)
+            {
+                ViewData["Error"] = "Couldn't update that course.";
+                return View(model);
+            }
+            else
+            {
+                context.Entry(model).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //var entity = context.Kourses.Find(id);
+            //if (entity != null)
             //{
+            //    //do the update
             //    return Redirect("/kourse");
             //}
-            var context = new KourseDbContext();
-            var entity = context.Kourses.Find(id);
-            if (entity != null)
+            ////return 404 page
+            //return View(model);
+        }
+
+        //GET: /Kourse/Delete/{id}
+        [HttpGet]
+        public ActionResult Delete(int? kourseId)
+        {
+            if(kourseId == null)
             {
-                //do the update
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var context = new KourseDbContext();
+            Kourse kourseToDelete = context.Kourses.Find(kourseId);
+            if (kourseToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kourseToDelete);
+        }
+
+        //POST: / Kourse/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int kourseId)
+        {
+            var context = new KourseDbContext();
+            Kourse kourseToDelete = context.Kourses.Find(kourseId);
+            context.Kourses.Remove(kourseToDelete);
+            int wasSuccess = context.SaveChanges();
+            if (wasSuccess == 1)
+            {
                 return Redirect("/kourse");
             }
-            //return 404 page
-            return View(model);
+            else
+            {
+                ViewData["Error"] = "Couldn't delete that course.";
+                return View();
+            }
         }
     }
 }
